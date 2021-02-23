4430a416ad89d9800986e78ee4b7d96e84b25c8b
databunnysg <yi@databunny.sg>
2021-02-24 01:21:19 +0800

    f

--- downloadtop2000.R
+++ downloadtop2000.R
@@ -3,5 +3,5 @@ for(i in 1:nrow(ranking))
 {
   if(file.exists(paste0("/mnt/webdownload/",ranking[i,]$channelid))) next
   system(paste0("/home/root/automate-save-page-as/save_page_as https://www.youtube.com/channel/",ranking[i,]$channelid,"/videos -b firefox -d /mnt/webdownload/",ranking[i,]$channelid))
-  Sys.sleep(80)
+  #Sys.sleep(2)
 }
