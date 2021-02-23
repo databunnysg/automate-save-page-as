a1ad1d9c08b050926ef061333c097eac579b44d2
databunnysg <yi@databunny.sg>
2021-02-24 00:55:17 +0800

    f

--- downloadtop2000.R
+++ downloadtop2000.R
@@ -2,6 +2,6 @@ ranking<-readRDS("chinesetop500categorized")
 #for(i in 1:nrow(ranking))
 i<-1
 {
-  system(paste0("/home/root/automate-save-page-as/save_page_as -b firefox -d /mnt/webdownload/",ranking[i,]$channelid))
+  system(paste0("/home/root/automate-save-page-as/save_page_as ",ranking[i,]$channelid," -b firefox -d /mnt/webdownload/",ranking[i,]$channelid))
   Sys.sleep(150)
 }
