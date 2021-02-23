27cf443440f689b78b2d2f1f4778353b19dce664
databunnysg <yi@databunny.sg>
2021-02-24 01:12:08 +0800

    f

--- downloadtop2000.R
+++ downloadtop2000.R
@@ -1,6 +1,5 @@
 ranking<-readRDS("chinesetop500categorized")
-#for(i in 1:nrow(ranking))
-i<-1
+for(i in 1:nrow(ranking))
 {
   system(paste0("/home/root/automate-save-page-as/save_page_as https://www.youtube.com/channel/",ranking[i,]$channelid,"/videos -b firefox -d /mnt/webdownload/",ranking[i,]$channelid))
   Sys.sleep(120)
