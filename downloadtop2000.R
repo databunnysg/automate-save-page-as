50ec21945f0c718689051c9d5a3d03b5d27f2513
databunnysg <yi@databunny.sg>
2021-02-24 01:18:28 +0800

    f

--- downloadtop2000.R
+++ downloadtop2000.R
@@ -1,6 +1,7 @@
 ranking<-readRDS("chinesetop500categorized")
 for(i in 1:nrow(ranking))
 {
+  if(file.exists(paste0("/mnt/webdownload/",ranking[i,]$channelid))) next
   system(paste0("/home/root/automate-save-page-as/save_page_as https://www.youtube.com/channel/",ranking[i,]$channelid,"/videos -b firefox -d /mnt/webdownload/",ranking[i,]$channelid))
-  Sys.sleep(120)
+  Sys.sleep(80)
 }
