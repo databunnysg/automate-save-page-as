8e9b2a215536c30ca89aa598dc4ca8ddadcea0f9
databunnysg <yi@databunny.sg>
2021-02-26 14:54:01 +0800

    f

--- downloadtop2000.R
+++ downloadtop2000.R
@@ -1,12 +1,12 @@
-ranking<-readRDS("chinesetop500categorized")
-for(i in 1:nrow(ranking))
-{
-  if(file.exists(paste0("/mnt/webdownload/",ranking[i,]$channelid))) next
-  cmd<-paste0("/home/root/automate-save-page-as/save_page_as https://www.youtube.com/channel/",ranking[i,]$channelid,"/videos -b firefox -d /mnt/webdownload/",ranking[i,]$channelid)
-  system(cmd)
-  cat(paste(cmd,"\n"))
-  #Sys.sleep(2)
-}
+# ranking<-readRDS("chinesetop500categorized")
+# for(i in 1:nrow(ranking))
+# {
+#   if(file.exists(paste0("/mnt/webdownload/",ranking[i,]$channelid))) next
+#   cmd<-paste0("/home/root/automate-save-page-as/save_page_as https://www.youtube.com/channel/",ranking[i,]$channelid,"/videos -b firefox -d /mnt/webdownload/",ranking[i,]$channelid)
+#   system(cmd)
+#   cat(paste(cmd,"\n"))
+#   #Sys.sleep(2)
+# }
 
 library(redux)
 library(jsonlite)
@@ -19,8 +19,8 @@ if(!exists("lconfig"))
   l$PING()
 }
 
-channel<-data.frame(channelid="UCPcF3KTqhD67ADkukx_OeDg",force=TRUE,pagedown=100,save_wait_time=30,stringsAsFactors = FALSE)
-l$RPUSH("youtubechannelloadtask",toJSON(channel))
+#channel<-data.frame(channelid="UCPcF3KTqhD67ADkukx_OeDg",force=TRUE,pagedown=100,save_wait_time=30,stringsAsFactors = FALSE)
+#l$RPUSH("youtubechannelloadtask",toJSON(channel))
 
 while(TRUE)
 {
