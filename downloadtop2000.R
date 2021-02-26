09924b580dc1d8df638630a56f8f5bf0cd0fdf22
databunnysg <yi@databunny.sg>
2021-02-26 13:25:36 +0800

    f

--- downloadtop2000.R
+++ downloadtop2000.R
@@ -2,6 +2,8 @@ ranking<-readRDS("chinesetop500categorized")
 for(i in 1:nrow(ranking))
 {
   if(file.exists(paste0("/mnt/webdownload/",ranking[i,]$channelid))) next
-  system(paste0("/home/root/automate-save-page-as/save_page_as https://www.youtube.com/channel/",ranking[i,]$channelid,"/videos -b firefox -d /mnt/webdownload/",ranking[i,]$channelid))
+  cmd<-paste0("/home/root/automate-save-page-as/save_page_as https://www.youtube.com/channel/",ranking[i,]$channelid,"/videos -b firefox -d /mnt/webdownload/",ranking[i,]$channelid)
+  system(cmd)
+  cat(paste(cmd,"\n"))
   #Sys.sleep(2)
 }
