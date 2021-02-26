5643ad117390f5737dbbcc9f958945eb644aecee
databunnysg <yi@databunny.sg>
2021-02-26 14:58:41 +0800

    f

--- downloadtop2000.R
+++ downloadtop2000.R
@@ -19,7 +19,7 @@ if(!exists("lconfig"))
   l$PING()
 }
 
-#channel<-data.frame(channelid="UCPcF3KTqhD67ADkukx_OeDg",force=TRUE,pagedown=100,save_wait_time=30,stringsAsFactors = FALSE)
+#channel<-data.frame(channelid="UCPcF3KTqhD67ADkukx_OeDg",force=TRUE,pagedown=100,savewaittime=30,stringsAsFactors = FALSE)
 #l$RPUSH("youtubechannelloadtask",toJSON(channel))
 
 while(TRUE)
@@ -34,7 +34,7 @@ while(TRUE)
   for(i in 1:nrow(channelobj))
   {
     if(!channelobj[i,]$force&&file.exists(paste0("/mnt/webdownload/",channelobj[i,]$channelid))) next
-    cmd<-paste0("/home/root/automate-save-page-as/save_page_as https://www.youtube.com/channel/",channelobj[i,]$channelid,"/videos -b firefox -d /mnt/webdownload/",channelobj[i,]$channelid," --pagedown ",channelobj[i,]$pagedown," --save_wait_time ",channelobj[i,]$save_wait_time)
+    cmd<-paste0("/home/root/automate-save-page-as/save_page_as https://www.youtube.com/channel/",channelobj[i,]$channelid,"/videos -b firefox -d /mnt/webdownload/",channelobj[i,]$channelid," --pagedown ",channelobj[i,]$pagedown," --savewaittime ",channelobj[i,]$savewaittime)
     system(cmd)
     cat(paste(cmd,"\n"))
     #Sys.sleep(2)
