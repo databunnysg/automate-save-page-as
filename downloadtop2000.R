8f47f32d7f4e2e2c0da2a619997fdf4811800cba
databunnysg <yi@databunny.sg>
2021-02-26 14:07:38 +0800

    add load channel from redis

--- downloadtop2000.R
+++ downloadtop2000.R
@@ -7,3 +7,36 @@ for(i in 1:nrow(ranking))
   cat(paste(cmd,"\n"))
   #Sys.sleep(2)
 }
+
+library(redux)
+library(jsonlite)
+if(!exists("lconfig"))
+{
+  lconfig<-redis_config(host="10.0.1.10",port="7001")
+  l<-hiredis(rconfig)
+  l$AUTH("REDISHOST")
+  l$SELECT(5)
+  l$PING()
+}
+
+#channel<-data.frame(channelid="UCPcF3KTqhD67ADkukx_OeDg",force=FALSE,pagedown=20,stringsAsFactors = FALSE)
+#l$RPUSH("youtubechannelloadtask",toJSON(channel))
+
+while(TRUE)
+{
+  pr<-l$RPOP("youtubechannelloadtask")
+  if(is.null(pr))
+  {
+    Sys.sleep(1)
+    next
+  }
+  channelobj<-fromJSON(pr)
+  for(i in 1:nrow(channelobj))
+  {
+    if(channelobj[i,]$force&&file.exists(paste0("/mnt/webdownload/",channelobj[i,]$channelid))) next
+    cmd<-paste0("/home/root/automate-save-page-as/save_page_as https://www.youtube.com/channel/",channelobj[i,]$channelid,"/videos -b firefox -d /mnt/webdownload/",channelobj[i,]$channelid," --pagedown ",channelobj[i,]$pagedown)
+    system(cmd)
+    cat(paste(cmd,"\n"))
+    #Sys.sleep(2)
+  }
+}
