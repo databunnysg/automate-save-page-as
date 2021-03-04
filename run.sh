f3bec91dd492fb2561958cf4f1d424d68d0788f2
databunnysg <yi@databunny.sg>
2021-03-04 15:10:14 +0800

    f

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 cd /home/root/automate-save-page-as/ && Rscript /home/root/automate-save-page-as/downloadvideocomments.R
 #cd /home/root/automate-save-page-as/ && Rscript /home/root/automate-save-page-as/downloadtop2000.R
